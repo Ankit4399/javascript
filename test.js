b0a4394181b937094427b57fb57784a08d34960c
Ankit yadav <142087805+Ankit4399@users.noreply.github.com>
2024-07-22 07:36:58 +0000

    conifg

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("hello")
+const accountId = "12434"
+let accountName = "ankit"
+let accountEmail = "ayay@google.com"
+let accountCity = "delhi"  //never use var
+//we cannot change const 
+accountName = "yadav"
+
+console.table([accountCity,accountEmail,accountId,accountName])
