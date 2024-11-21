3e21669f8f0e411216f4ce1977d9e5df2c2c9d21
Ankit yadav <142087805+Ankit4399@users.noreply.github.com>
2024-11-21 18:52:08 +0000

    datatypes done

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+let name = "ankit"
+let age = 20
+let isLoggedin = true
+
+
+let state;
+let undefined;
+//undefined is something you dont define like let state;
+// null means empty it is a object;
+
+console.log(typeof "ankit");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
