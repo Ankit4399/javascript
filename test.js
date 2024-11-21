b596d5e80b3a637a6648cef6cfc5c99c4e42584a
Ankit yadav <142087805+Ankit4399@users.noreply.github.com>
2024-11-21 11:30:36 +0000

    config done

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const accountId = "12434"
 let accountName = "ankit"
 let accountEmail = "ayay@google.com"
-let accountCity = "delhi"  //never use var
+let accountCity = "delhi"  //never use var because of issue in block scope and functional scope
 //we cannot change const 
-accountName = "yadav"
+accountName = "yadav" // bad practice
+let accountState;
 
-console.table([accountCity,accountEmail,accountId,accountName])
+console.table([accountCity,accountEmail,accountId,accountName,accountState])
