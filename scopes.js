77800c6965baf57480294e6da2f2003b98ae6795
Ankit yadav <142087805+Ankit4399@users.noreply.github.com>
2024-12-08 18:11:19 +0000

    scopes and mini hoisting

---
+++ scopes.js
@@ -0,0 +1,59 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){           //if we access function before initalisation in this type of function declaration, it works fine.
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){   //but in terms of variable it throws error.
+    return num + 2
+}
